d2f0908d1b763e44071034eca28de9b8e5d21967
Jorge Partal Bravo <jorgepartal@gmail.com>
2016-07-14 12:54:24 -0400

    Se agrego una activity que leerá un json

--- app/src/main/java/com/codebake/com/aplicatodo/MainActivity.java
+++ app/src/main/java/com/codebake/com/aplicatodo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codebake.com.aplicatodo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,17 +81,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_json) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+            Intent lector_json = new Intent(this, JsonActivity.class);
+            startActivity(lector_json);
 
         }
 
