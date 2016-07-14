ae3a2186953aecbbd039ac88d99f423eba008b16
Jorge Partal Bravo <jorgepartal@gmail.com>
2016-07-14 12:19:04 -0400

    Primero creamos la activity para el splash

---
+++ app/src/main/java/com/codebake/com/aplicatodo/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.codebake.com.aplicatodo;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, 3000);
+
+
+
+    }
+}
